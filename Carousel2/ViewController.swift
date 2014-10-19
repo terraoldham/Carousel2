e5892d6e5d57cb91f7d5635ffb46df686b364662
Terra Oldham <toldham@hearsaycorp.com>
2014-10-19 13:09:46 -0700

    Initial Commit

---
+++ Carousel2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Carousel2
+//
+//  Created by Terra Oldham on 10/19/14.
+//  Copyright (c) 2014 HearsaySocial. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
