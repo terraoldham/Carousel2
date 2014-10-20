5025576198639b87d9eefd4806dc82f0191f3079
Terra Oldham <toldham@hearsaycorp.com>
2014-10-19 23:59:27 -0700

    Sign Up Flow Completed
    Sign Up Flow Completed

--- Carousel2/CreateViewController.swift
+++ Carousel2/CreateViewController.swift
@@ -14,7 +14,9 @@ class CreateViewController: UIViewController {
     @IBOutlet weak var firstField: UITextField!
     @IBOutlet weak var lastField: UITextField!
     @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var createButton: UIButton!
     @IBOutlet weak var passwordField: UITextField!
+    @IBAction func onAcceptButton(sender: AnyObject) {createButton.selected = true}
     @IBAction func createButton(sender: AnyObject) {
         checkCreate()}
     override func viewDidLoad() {
@@ -32,9 +34,20 @@ class CreateViewController: UIViewController {
         if emailField.text.isEmpty || firstField.text.isEmpty || lastField.text.isEmpty || passwordField.text.isEmpty {
             var createAlertView = UIAlertView(title: "Enter Fields", message: "Please enter your name, email, and password.", delegate: nil, cancelButtonTitle: "OK")
             createAlertView.show()
+        } else if createButton.selected == false {
+            var termsAlert = UIAlertView(title: "Terms of Use", message: "Please accept the Terms of Use before creating an account.", delegate: nil, cancelButtonTitle: "OK")
+            termsAlert.show()
         } else {
-            self.performSegueWithIdentifier("create segue", sender: self)
+            var createAlert = UIAlertView(title: "Creating Account", message: nil, delegate: nil, cancelButtonTitle: nil)
+            delay(2){
+                createAlert.dismissWithClickedButtonIndex(0, animated: true)}
+            createAlert.show()
+            performSegueWithIdentifier("create segue", sender: self)
         }
+    }
+    
+    func delay(delay:Double, closure:()->()) {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC))), dispatch_get_main_queue(), closure)
     }
 
     /*
