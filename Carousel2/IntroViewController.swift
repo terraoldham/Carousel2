6db52e55942fd492872bfc1a1dab630989d48c1a
Terra Oldham <toldham@hearsaycorp.com>
2014-10-19 17:18:47 -0700

    Carousel Take 2
    Initial Commit

---
+++ Carousel2/IntroViewController.swift
@@ -0,0 +1,38 @@
+//
+//  IntroViewController.swift
+//  Carousel2
+//
+//  Created by Terra Oldham on 10/19/14.
+//  Copyright (c) 2014 HearsaySocial. All rights reserved.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.contentSize = CGSize(width: 320, height: 1190)
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Carousel2/SignInViewController.swift
@@ -0,0 +1,65 @@
+//
+//  SignInViewController.swift
+//  Carousel2
+//
+//  Created by Terra Oldham on 10/19/14.
+//  Copyright (c) 2014 HearsaySocial. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    @IBOutlet weak var loginActivity: UIActivityIndicatorView!
+    @IBAction func arrowsButton(sender: AnyObject) {navigationController?.popToRootViewControllerAnimated(false)}
+    @IBAction func signinButton(sender: AnyObject) {
+        self.loginActivity.startAnimating()
+        delay(2){
+            self.checkFields()
+            self.loginActivity.stopAnimating()
+            }
+        }
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBAction func onTap(sender: AnyObject) {view.endEditing(true)
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func checkFields() {
+        if self.emailField.text == "email" && self.passwordField.text == "password" {
+            self.performSegueWithIdentifier("signin", sender: self)
+        } else if self.emailField.text == "" && self.passwordField.text == "" {
+            var alertViewBlank = UIAlertView(title: "Blank Fields", message: "Please enter the correct credentials.", delegate: nil, cancelButtonTitle: "OK")
+            alertViewBlank.show()
+        } else {
+            var alertView = UIAlertView(title: "Sign In Error", message: "Please enter the correct credentials.", delegate: nil, cancelButtonTitle: "OK")
+            alertView.show()
+        }
+    }
+    
+    func delay(delay:Double, closure:()->()) {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC))), dispatch_get_main_queue(), closure)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
